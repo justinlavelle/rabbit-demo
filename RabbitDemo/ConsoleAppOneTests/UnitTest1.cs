0f4614dc2e5eb54a6d0252857cfacee414401250
Justin Lavelle <justinlavelle@black-glass.com>
2019-06-21 15:16:40 +0200

    Initial Commit
    Initial Commit with broader project Structure

---
+++ RabbitDemo/ConsoleAppOneTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using ConsoleAppOne;                        // IDE Moans about this directive being unnecessary
+using System;
+using Xunit;
+
+namespace ConsoleAppOneTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
